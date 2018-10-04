ae5a77baca4ef9a24845acc8c96db64b8b1b330a
yhtomitsy <yhtosoft@gmail.com>
2018-10-04 18:34:26 +0700

    serial control with wave and duration

---
+++ DRV2605_i2C.ino
@@ -0,0 +1,33 @@
+/*bool DRVInit() 
+{
+    //Get a read from the status register
+    //Want this to Read 0xE0, any other value than 0 and you have tripped the over-current protection=wrong motor
+    uint8_t status = readDRV2605L(STATUS_REG);
+    
+    Serial.print("Status Register 0x"); 
+    Serial.println(status, HEX);
+    return true;
+}
+
+//Read function
+uint8_t readDRV2605L(uint8_t reg) 
+{
+    uint8_t var ;
+    // Enable I2C Read
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write((byte)reg);
+    Wire.endTransmission();
+    Wire.requestFrom((byte)I2C_ADDR, (byte)1);
+    var = Wire.read();
+    return var;
+}
+
+//Write Function 
+void writeDRV2605L(uint8_t where, uint8_t what)
+{
+   // Enable I2C write
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write((byte)where);
+    Wire.write((byte)what);
+    Wire.endTransmission();
+}*/
--- haptics_effects_test.ino
+++ haptics_effects_test.ino
@@ -2,13 +2,21 @@
 #include <Wire.h> //I2C library 
 
 SFE_HMD_DRV2605L HMD; //Create haptic motor driver object 
-int wave[2] = {0};
-int sequence = 0;
+
+
 #define trig_Pin 2
 #define waitTime 50
+#define I2C_ADDR 0x5A
 
 uint32_t timeTrack = 0;
 boolean triggered = false;
+int wave[2] = {0};
+int sequence = 0;
+uint8_t finger = 0;
+uint8_t data_index = 0;
+int duration = 0;
+
+
 
 void setup() 
 {
@@ -20,43 +28,76 @@ void setup()
   Wire.setClock(400000); //Increase I2C data rate to 400kHz
   
   HMD.begin();
-  HMD.Mode(0); // Internal trigger input mode -- Must use the GO() function to trigger playback.
-  HMD.MotorSelect(0x36); // ERM motor, 4x Braking, Medium loop gain, 1.365x back EMF gain
-  HMD.Library(2);       //1-5 & 7 for ERM motors, 6 for LRA motors 
+  HMD.Mode(0);           // Internal trigger input mode -- Must use the GO() function to trigger playback.
+  HMD.MotorSelect(0xB9); //0x36 // ERM motor, 4x Braking, Medium loop gain, 1.365x back EMF gain
+  HMD.Library(6);        //1-5 & 7 for ERM motors, 6 for LRA motors 
 
 }
 void loop() 
 {
-  uint32_t t = millis();
-  wave[0] = 0;
+  //uint32_t t = millis(); 
   if(Serial.available()){
+      duration = 0;
       int buff = Serial.available();
       for(uint8_t i = 0; i < buff; i++){
           char c = (char)Serial.read();
-          if(c != ';' && c != '\r' && c != '\n'){
-              wave[0] = (wave[0] * 10) + c - '0'; // Get num from somewhere
+          if(c == '\r')
+          {
+             // do nothing 
           }
-          if(c == '\r') 
-          if(c == '\n')break;           
+          else if(c == '\n')
+          {
+              break;
+          }
+          else if(c == ';')
+          {
+              data_index++;
+          }
+          else{
+              switch(data_index)
+              {
+                  case 0:
+                      finger = (finger * 10) +  c - '0';
+                      break;
+                  case 1: 
+                      wave[0] = (wave[0] * 10) + c - '0'; 
+                      break;
+                  case 2: 
+                      duration = (duration * 10) + c - '0';
+                      break;
+                  default:
+                      break;
+              }
+              
+          }
+                     
       }
-      Serial.print("wave: ");Serial.println(wave[0]);
+     
       if(wave[0] > 64)sequence = int((wave[0] - 64) / 8);
       else sequence = int(wave[0] / 8);
+
+      Serial.print("Finger: "); Serial.println(finger);
       Serial.print("sequence: ");Serial.println(sequence);
+      Serial.print("wave: ");Serial.println(wave[0]);
+      Serial.print("Duration: ");Serial.println(duration);
+      
       if(wave[0] > 0){
-        HMD.Waveform(sequence, wave[0]);
-        HMD.go();
-        timeTrack = millis();
-        triggered = true;
-      }
+          HMD.Waveform(sequence, wave[0]);
+          HMD.go();
+          timeTrack = millis();
+          triggered = true;
+      }   
+      
+      data_index = 0;
+      finger = 0;
+      wave[0] = 0;
   } 
-  if(millis() - timeTrack >= waitTime && triggered){
+  if(millis() - timeTrack >= duration && triggered){
       HMD.stop();
-      //timeTrack = millis();
-      //Serial.println("kamaliza");
+      //Serial.println(duration);
       triggered = false;
   }
-  Serial.println(1000 / (millis() - t));
+  //Serial.println(1000 / (millis() - t));
 }
 
  
